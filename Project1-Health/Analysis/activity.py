ac02eca791f37640f4e242c2a30c4c2a020db627
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-01-27 10:45:59 +0000

    Cleaned up, added comments, reformatted

--- Project1-Health/Analysis/activity.py
+++ Project1-Health/Analysis/activity.py
@@ -2,11 +2,8 @@
 #
 # Total activity and duration for each cell tower (incoming + outgoing calls)
 #
-# Input: hourly-stamped csv files of CDR data
-# Output: a 1240 x 1 array containing the total activity and duration of each cell tower (10 of these entries will
-#     remain at 0 as the IDs do not correspond to actual towers. Some others will remain at 0 despite corresponding
-#     to real towers; they are just never active. I try to discard as many of these as possible in the subsequent
-#     processing stage. I also remove towers that become active after months of inactivity.
+# Input: hourly time-stamped csv files of CDR data
+# Output: a (num_towers) x 1 array containing the total activity and duration of each cell tower
 
 import pandas as pd
 import numpy as np
@@ -14,28 +11,38 @@ import math
 
 if __name__ == "__main__":
 
+    path = "/Users/JackShipway/Desktop/UCLProject"
+
+    # country = sys.argv[1]
+    country = 'Senegal'
+
+    # Set known data set values (number of towers, and time length of data)
+    if country == 'Senegal':
+        num_bts, hours = 1668, 8760
+    elif country == 'IvoryCoast':
+        num_bts, hours = 1240, 3360
+    else:
+        num_bts, hours = 10000, 100000
+
+    # Starting time-step (month, week, day, hour)
     m, w, d, h = 0, 0, 0, 0
 
-    volume_total = np.zeros(1667)
-    volume_in = np.zeros(1667)
-    volume_out = np.zeros(1667)
-
-    duration_total = np.zeros(1667)
-    duration_in = np.zeros(1667)
-    duration_out = np.zeros(1667)
+    # Metrics to derive
+    volume_total, volume_in, volume_out = np.zeros(num_bts), np.zeros(num_bts), np.zeros(num_bts)
+    duration_total, duration_in, duration_out = np.zeros(num_bts), np.zeros(num_bts), np.zeros(num_bts)
 
     # Sum output for each cell tower, for each hour of data
-    for hour in range(8760):
-        print "Reading Hour: %s" % hour
-        # Read each of the temporal data sets in turn, convert to multi-dimensional array
-        data = pd.read_csv("Data/Senegal/CDR/Temporal/Month_%s/Week_%s/Day_%s/Hour_%s/graph.csv" % (m, w, d, h),
+    for hour in range(hours):
+        print "Reading Hour: %s" % hour, m, w, d, h
+
+        # Read the temporal data sets in turn, convert to multi-dimensional array
+        data = pd.read_csv(path+"/Data/%s/CDR/Temporal/Month_%s/Week_%s/Day_%s/Hour_%s/graph.csv" % (country, m, w, d, h),
                            usecols=["source", "target", "volume", "duration"]).as_matrix()
 
-        # Only  cell towers active within that time-step
+        # Only update cell towers active within that time-step
         active_towers = np.array(np.unique(np.concatenate([data[:, 0], data[:, 1]])))
 
         for cell_tower in active_towers:
-
             active_data = data[data[:, 0] == cell_tower]
 
             in_vol = np.sum(active_data[:, 2])
@@ -44,22 +51,20 @@ if __name__ == "__main__":
             in_dur = np.sum(active_data[:, 3])
             out_dur = np.sum(data[data[:, 1] == cell_tower][:, 3])
 
+            # Taking into account (i, i) counted twice, so it must be subtracted once
             if active_data.size == 0:
-                self_vol = 0
-                self_dur = 0
+                self_vol, self_dur = 0, 0
             else:
                 self_active = active_data[active_data[:, 1] == cell_tower]
                 if self_active.size != 0:
-                    self_vol = self_active[0][2]
-                    self_dur = self_active[0][3]
+                    self_vol, self_dur = self_active[0][2], self_active[0][3]
                 else:
-                    self_vol = 0
-                    self_dur = 0
+                    self_vol, self_dur = 0, 0
 
+            # Increment activity for respective cell towers
             volume_total[cell_tower] += (in_vol + out_vol) - self_vol
             volume_in[cell_tower] += in_vol
             volume_out[cell_tower] += out_vol
-
             duration_total[cell_tower] += (in_dur + out_dur) - self_dur
             duration_in[cell_tower] += in_dur
             duration_out[cell_tower] += out_dur
@@ -82,4 +87,5 @@ if __name__ == "__main__":
     total_activity['Dur_in'] = duration_in
     total_activity['Dur_out'] = duration_out
 
-    total_activity.to_csv('Data/Senegal/CDR/Metrics/total_activity.csv', index=None)
+    # # Save results
+    # total_activity.to_csv("total_activity.csv", index=None)
