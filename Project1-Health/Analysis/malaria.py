b7b48f9e575a2fa0ed153276cc00c395041815dd
Jack Shipway <jack.shipway@breakthemold.co.uk>
2017-01-27 13:39:02 +0000

    explained what this file does, can use for SPSS files in general, helpful applymap method

--- Project1-Health/Analysis/malaria.py
+++ Project1-Health/Analysis/malaria.py
@@ -1,15 +1,19 @@
+''' This file shows how to take engineered DHS data, and extract various metrics such as number of malaria cases '''
+# Inputs:
+# Outputs:
+
 import pandas as pd
 import numpy as np
 
 if __name__ == "__main__":
 
-    ''' Malaria-based spatial metrics '''
-
+    # Data set selected, trimmed down, and weighted from SPSS
     malaria_dhs = pd.DataFrame(pd.read_csv("Data/IvoryCoast/DHS/Extracted/malaria_weighted.csv"))
+    # Convert strings to numeric values for all data
     malaria_dhs = malaria_dhs.applymap(lambda x: 0 if isinstance(x, basestring) and x.isspace() else x)
-
     data = malaria_dhs.as_matrix().astype(int)
 
+    # values of 6,7,9 would indicate problems with the sampling
     data = np.delete(data, np.where(data[:, 4:40] == 6)[0], axis=0)
     data = np.delete(data, np.where(data[:, 4:40] == 7)[0], axis=0)
     data = np.delete(data, np.where(data[:, 4:40] == 9)[0], axis=0)
@@ -27,7 +31,7 @@ if __name__ == "__main__":
     cluster_cases = cluster_cases.ix[cluster_adm4['DHSCLUST']]
 
     cluster_cases['ADM_4'] = np.array(cluster_adm4['ID_4'])
-    print cluster_cases
+
 
     cluster_cases = cluster_cases.groupby('ADM_4')['CasePerPerson'].mean()
     cluster_cases = pd.DataFrame(cluster_cases.reindex(range(192), fill_value=0))
